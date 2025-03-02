371ef20bddc5a8d10a7f420a27ab1825f8b7e4e0
Sakyrhythm <438399478@qq.com>
2025-03-02 15:01:54 +0800

    模版

---
+++ src/main/java/com/sakyrhythm/psychosis/PsychosisModClient.java
@@ -0,0 +1,10 @@
+package com.sakyrhythm.psychosis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PsychosisModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
