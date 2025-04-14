09111ae3cd1405b0d5d676345f2ad2832e49af48
pltision <mciduu@163.com>
2025-04-14 23:10:26 +0800

    复制玩家模型，加载在线玩家贴图给模型

--- src/main/java/com/sakyrhythm/psychosis/entity/client/ModModelLayers.java
+++ src/main/java/com/sakyrhythm/psychosis/entity/client/ModModelLayers.java
@@ -5,6 +5,8 @@ import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.util.Identifier;
 
 public class ModModelLayers {
-    public static final EntityModelLayer PLAYER =
-            new EntityModelLayer(Identifier.of(Psychosis.MOD_ID,"player"),"main");
+    public static final EntityModelLayer PLAYER_STEVE =
+            new EntityModelLayer(Identifier.of(Psychosis.MOD_ID,"player_steve"),"main");
+    public static final EntityModelLayer PLAYER_SLIM =
+            new EntityModelLayer(Identifier.of(Psychosis.MOD_ID,"player_slim"),"main");
 }
