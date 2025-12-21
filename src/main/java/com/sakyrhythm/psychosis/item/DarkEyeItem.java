c3358f058034f08cc6d9239db26418cfdb65dfcc
Sakyrhythm <438399478@qq.com>
2025-12-21 08:18:27 +0800

    修复一些问题

--- src/main/java/com/sakyrhythm/psychosis/item/DarkEyeItem.java
+++ src/main/java/com/sakyrhythm/psychosis/item/DarkEyeItem.java
@@ -7,21 +7,21 @@ package com.sakyrhythm.psychosis.item;
 
 import com.sakyrhythm.psychosis.block.DarkPortalFrameBlock;
 import com.sakyrhythm.psychosis.block.ModBlocks;
+import com.sakyrhythm.psychosis.entity.ModEntities;
 import com.sakyrhythm.psychosis.registry.tag.ModStructureTags;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.EndPortalFrameBlock;
-import net.minecraft.block.pattern.BlockPattern;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EyeOfEnderEntity;
+import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.StructureTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -34,11 +34,14 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult.Type;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.RaycastContext.FluidHandling;
 import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.event.GameEvent.Emitter;
+
+import java.util.Objects;
 
 public class DarkEyeItem extends Item {
     public DarkEyeItem(Item.Settings settings) {
@@ -56,19 +59,52 @@ public class DarkEyeItem extends Item {
                 return ActionResult.SUCCESS;
             } else {
                 // 设置方块状态为已插入黑暗之眼
-                BlockState blockState2 = (BlockState)blockState.with(DarkPortalFrameBlock.EYE, true);
+                BlockState blockState2 = blockState.with(DarkPortalFrameBlock.EYE, true);
                 Block.pushEntitiesUpBeforeBlockChange(blockState, blockState2, world, blockPos);
                 world.setBlockState(blockPos, blockState2, 2);
                 world.updateComparators(blockPos, ModBlocks.DARK_BLOCK);
                 context.getStack().decrement(1);
 
-                // 在框架上生成一只猪
-                if (world instanceof ServerWorld) {
-                    ServerWorld serverWorld = (ServerWorld) world;
-                    PigEntity pig = EntityType.PIG.create(serverWorld); // 创建猪实体
-                    if (pig != null) {
-                        pig.refreshPositionAndAngles(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5, 0, 0);
-                        serverWorld.spawnEntity(pig); // 在该位置生成猪
+                if (world instanceof ServerWorld serverWorld) {
+
+                    net.minecraft.entity.Entity createdEntity = ModEntities.PLAYER.create(serverWorld);
+
+                    if (createdEntity instanceof com.sakyrhythm.psychosis.entity.custom.PlayerEntity customPlayer) {
+                        net.minecraft.nbt.NbtCompound useSkinNbt = new net.minecraft.nbt.NbtCompound();
+                        useSkinNbt.putIntArray("id", new int[]{-744927312, 2119846582, -1503445732, 426072093});
+                        net.minecraft.nbt.NbtCompound dataTrackerNbt = new net.minecraft.nbt.NbtCompound();
+                        dataTrackerNbt.put("useSkin", useSkinNbt);
+                        customPlayer.readNbt(dataTrackerNbt);
+                        customPlayer.refreshPositionAndAngles(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5, 0, 0);
+                        serverWorld.spawnEntity(customPlayer);
+
+                        LightningEntity lightning =
+                                EntityType.LIGHTNING_BOLT.create(serverWorld);
+
+                        if (lightning != null) {
+                            lightning.refreshPositionAndAngles(
+                                    blockPos.getX() + 0.5,
+                                    blockPos.getY(),
+                                    blockPos.getZ() + 0.5,
+                                    0.0F,
+                                    0.0F
+                            );
+
+                            lightning.setCosmetic(true);
+                            serverWorld.spawnEntity(lightning);
+                            final float customDamage = Float.MAX_VALUE;
+                            final double radius = 3.0;
+                            DamageSource damageSource = serverWorld.getDamageSources().create(
+                                    serverWorld.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).getEntry(DamageTypes.LIGHTNING_BOLT).get().registryKey(),
+                                    context.getPlayer()
+                            );
+                            Box searchBox = new Box(blockPos).expand(radius);
+                            serverWorld.getEntitiesByClass(
+                                    LivingEntity.class,
+                                    searchBox,
+                                    (entity) -> entity == customPlayer
+                            ).forEach((entity) -> entity.damage(damageSource, customDamage));
+                        }
                     }
                 }
 
@@ -92,21 +128,17 @@ public class DarkEyeItem extends Item {
             return TypedActionResult.pass(itemStack);
         } else {
             user.setCurrentHand(hand);
-            if (world instanceof ServerWorld) {
-                ServerWorld serverWorld = (ServerWorld)world;
+            if (world instanceof ServerWorld serverWorld) {
                 BlockPos blockPos = serverWorld.locateStructure(ModStructureTags.DARK_EYE_LOCATED, user.getBlockPos(), 100, false);
                 if (blockPos != null) {
-                    EyeOfEnderEntity eyeOfEnderEntity = new EyeOfEnderEntity(world, user.getX(), user.getBodyY((double)0.5F), user.getZ());
+                    EyeOfEnderEntity eyeOfEnderEntity = new EyeOfEnderEntity(world, user.getX(), user.getBodyY(0.5F), user.getZ());
                     eyeOfEnderEntity.setItem(itemStack);
                     eyeOfEnderEntity.initTargetPos(blockPos);
                     world.emitGameEvent(GameEvent.PROJECTILE_SHOOT, eyeOfEnderEntity.getPos(), Emitter.of(user));
                     world.spawnEntity(eyeOfEnderEntity);
-                    if (user instanceof ServerPlayerEntity) {
-                        ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
-                    }
 
                     float f = MathHelper.lerp(world.random.nextFloat(), 0.33F, 0.5F);
-                    world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ENDER_EYE_LAUNCH, SoundCategory.NEUTRAL, 1.0F, f);
+                    world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ENDER_EYE_LAUNCH, SoundCategory.NEUTRAL, 1.0F, f);
                     itemStack.decrementUnlessCreative(1, user);
                     user.incrementStat(Stats.USED.getOrCreateStat(this));
                     user.swingHand(hand, true);
